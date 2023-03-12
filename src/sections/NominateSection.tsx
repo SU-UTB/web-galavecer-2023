d74366d189b926a11f1d195529b472db02a8777e
Filip Tomeš <fitome@ttu.ee>
2023-03-13 00:31:29 +0200

    fix: replaced link

--- src/sections/NominateSection.tsx
+++ src/sections/NominateSection.tsx
@@ -1,10 +1,11 @@
+import NominateLink from '../components/actionLink/NominateLink';
 import VoteLink from '../components/actionLink/VoteLink';
 import { StyledVoteSection } from './Sections.styled';
 
 const NominateSection = () => {
   return (
     <StyledVoteSection id="nominuj">
-      <VoteLink />
+      <NominateLink />
       <p>
         Nominace končí 21.3. 2023
         <br />
