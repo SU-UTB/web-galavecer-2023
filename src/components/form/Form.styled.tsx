1145b1b6cb0a10e4a3ee9052486131d76ccf8bc2
Filip Tomeš <fitome@ttu.ee>
2023-03-13 21:32:28 +0200

    form styling - layout

--- src/components/form/Form.styled.tsx
+++ src/components/form/Form.styled.tsx
@@ -1,41 +1,83 @@
 import styled from 'styled-components';
 
 export const StyledForm = styled.form`
-  font-family: ${({ theme }) => theme.fontFamilies.headlineRegular}
-  width: 100%;
+  font-family: ${({ theme }) => theme.fontFamilies.headlineRegular};
   max-width: ${({ theme }) => theme.breakpoints.desktop};
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   margin: 4rem 0;
-  gap: 2rem 0;
+  gap: 3rem 0;
+  min-height: 60vh;
+  align-items: center;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding: 0 2rem;
+  }
 
   input,
   select,
   textarea {
-    width: 100%;
-    padding: 0.5rem 0.25rem;
+    padding: 0.8rem 0.5rem;
     border-radius: 0.5rem;
     outline: none;
     background: #ffffff;
     border: none;
+    margin-bottom: 0.5rem;
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+      min-width: 300px;
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export const StyledFormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem 0;
+`;
+
+export const StyledFormRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 0;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    gap: 0 1rem;
   }
 `;
 
 export const StyledInput = styled.input`
-  margin-bottom: 0.5rem;
+  width: 100%;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    width: auto;
+  }
 `;
 
 export const StyledSelect = styled.select`
-  margin-bottom: 0.5rem;
+  width: 100%;
+`;
+
+export const StyledTextarea = styled.textarea`
+  width: 100%;
 `;
 
 export const StyledSubmitButton = styled.button`
-  padding: 1rem 0.5rem;
   background: #ffffff;
   color: #000000;
   text-align: center;
-  text-transform: uppercase;
+  padding: 1rem 1.8rem;
   border-radius: 23px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  text-decoration: none;
+  font-family: ${({ theme }) => theme.fontFamilies.headlineSemibold};
+  font-size: 36px;
+  font-weight: 600;
   border: none;
+  max-width: 300px;
+  align-self: flex-start;
 `;
--- src/components/form/FormNomination.tsx
+++ src/components/form/FormNomination.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import { IFormNominationData } from '../../lib/interfaces/IFormNominationData';
 import {
   StyledForm,
+  StyledFormGroup,
+  StyledFormRow,
   StyledInput,
   StyledSelect,
   StyledSubmitButton,
+  StyledTextarea,
 } from './Form.styled';
 
 const INITIAL_DATA: IFormNominationData = {
@@ -35,85 +38,93 @@ const FormNomination = () => {
 
   return (
     <StyledForm onSubmit={submitForm}>
-      <div>
-        <StyledInput
-          type="text"
-          name="firstNameNominated"
-          placeholder="Jméno nominovaného"
-          required
-          value={data.firstNameNominated}
-          onChange={(e) => updateFields({ firstNameNominated: e.target.value })}
-        />
-        <StyledInput
-          type="text"
-          name="lastNameNominated"
-          placeholder="Příjmení nominovaného"
-          required
-          value={data.lastNameNominated}
-          onChange={(e) => updateFields({ lastNameNominated: e.target.value })}
-        />
-      </div>
-      <div>
-        <StyledSelect
-          name="facultyNominated"
-          id="facultyNominated"
-          required
-          value={data.facultyNominated}
-          onChange={(e) =>
-            updateFields({ facultyNominated: Number(e.target.value) })
-          }
-        >
-          <option value="0" disabled>
-            Fakulta nominovaného
-          </option>
-          <option value="1">FAI</option>
-          <option value="2">FMK</option>
-          <option value="3">FHS</option>
-        </StyledSelect>
-      </div>
-      <div>
-        <textarea
-          name="achievementsNominated"
-          id="achievementsNominated"
-          placeholder="Dúvod k nominaci"
-          required
-          value={data.achievementsNominated}
-          onChange={(e) =>
-            updateFields({ achievementsNominated: e.target.value })
-          }
-        ></textarea>
-      </div>
-      <div>
-        <StyledInput
-          type="text"
-          placeholder="Jméno"
-          name="firstName"
-          id="firstName"
-          required
-          value={data.firstName}
-          onChange={(e) => updateFields({ firstName: e.target.value })}
-        />
-        <StyledInput
-          type="text"
-          placeholder="Příjmení"
-          name="lastName"
-          id="lastName"
-          required
-          value={data.lastName}
-          onChange={(e) => updateFields({ lastName: e.target.value })}
-        />
-      </div>
-      <div>
-        <StyledInput
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Univerzitní e-mail"
-          required
-          value={data.email}
-          onChange={(e) => updateFields({ email: e.target.value })}
-        />
-      </div>
+      <StyledFormGroup>
+        <StyledFormRow>
+          <StyledInput
+            type="text"
+            name="firstNameNominated"
+            placeholder="Jméno nominovaného"
+            required
+            value={data.firstNameNominated}
+            onChange={(e) =>
+              updateFields({ firstNameNominated: e.target.value })
+            }
+          />
+          <StyledInput
+            type="text"
+            name="lastNameNominated"
+            placeholder="Příjmení nominovaného"
+            required
+            value={data.lastNameNominated}
+            onChange={(e) =>
+              updateFields({ lastNameNominated: e.target.value })
+            }
+          />
+        </StyledFormRow>
+        <StyledFormRow>
+          <StyledSelect
+            name="facultyNominated"
+            id="facultyNominated"
+            required
+            value={data.facultyNominated}
+            onChange={(e) =>
+              updateFields({ facultyNominated: Number(e.target.value) })
+            }
+          >
+            <option value="0" disabled>
+              Fakulta nominovaného
+            </option>
+            <option value="1">FAI</option>
+            <option value="2">FMK</option>
+            <option value="3">FHS</option>
+          </StyledSelect>
+        </StyledFormRow>
+        <StyledFormRow>
+          <StyledTextarea
+            name="achievementsNominated"
+            id="achievementsNominated"
+            placeholder="Dúvod k nominaci"
+            required
+            value={data.achievementsNominated}
+            onChange={(e) =>
+              updateFields({ achievementsNominated: e.target.value })
+            }
+          ></StyledTextarea>
+        </StyledFormRow>
+      </StyledFormGroup>
+      <StyledFormGroup>
+        <StyledFormRow>
+          <StyledInput
+            type="text"
+            placeholder="Jméno"
+            name="firstName"
+            id="firstName"
+            required
+            value={data.firstName}
+            onChange={(e) => updateFields({ firstName: e.target.value })}
+          />
+          <StyledInput
+            type="text"
+            placeholder="Příjmení"
+            name="lastName"
+            id="lastName"
+            required
+            value={data.lastName}
+            onChange={(e) => updateFields({ lastName: e.target.value })}
+          />
+        </StyledFormRow>
+        <StyledFormRow>
+          <StyledInput
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Univerzitní e-mail"
+            required
+            value={data.email}
+            onChange={(e) => updateFields({ email: e.target.value })}
+          />
+        </StyledFormRow>
+      </StyledFormGroup>
       <StyledSubmitButton type="submit">Nominovat</StyledSubmitButton>
     </StyledForm>
   );
