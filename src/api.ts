bb57fa3662ad38d95401ea0991bfb7916e79b05d
Filip Tomeš <fitome@ttu.ee>
2023-03-14 21:31:27 +0200

    updated api base url

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://sdtest.wz.cz/api';
+const API_BASE_URL = 'https://sdtest.wz.cz/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
