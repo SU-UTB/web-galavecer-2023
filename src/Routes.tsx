f71b459136459dcb0a11e8b9cbb9d073dae11653
Filip Tomeš <fitome@ttu.ee>
2023-03-21 15:17:21 +0200

    voting setup

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
-import NominationFormPage from './pages/NominationFormPage';
 import PartnersPage from './pages/PartnersPage';
 import PersonalInfoPage from './pages/PersonalInfoPage';
 import RulesPage from './pages/RulesPage';
+import VotingFormPage from './pages/VotingFormPage';
 
 const Routes = () => {
   return (
@@ -11,7 +11,7 @@ const Routes = () => {
       <Route index path="/" element={<MainPage />} />
       <Route path="/pravidla" element={<RulesPage />} />
       <Route path="/osobni-udaje" element={<PersonalInfoPage />} />
-      <Route path="/nominace" element={<NominationFormPage />} />
+      <Route path="/hlasuj" element={<VotingFormPage />} />
       <Route path="/partneri" element={<PartnersPage />} />
     </RouterRoutes>
   );
---
+++ src/pages/VotingFormPage.tsx
@@ -0,0 +1,11 @@
+import FormVoting from '../components/form/FormVoting';
+
+const VotingFormPage = () => {
+  return (
+    <>
+      <FormVoting />
+    </>
+  );
+};
+
+export default VotingFormPage;
